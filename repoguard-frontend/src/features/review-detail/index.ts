8df8c569bc7c10a3ee69435ad3fc20079c673cf9
cocoyu <874522742@qq.com>
2026-06-19 08:04:46 +0800

    refactor: extract review detail github comments card

--- repoguard-frontend/src/features/review-detail/index.ts
+++ repoguard-frontend/src/features/review-detail/index.ts
@@ -1,4 +1,5 @@
 export { default as ReviewDetailFilesSection } from "./components/ReviewDetailFilesSection.vue";
+export { default as ReviewDetailGithubCommentsCard } from "./components/ReviewDetailGithubCommentsCard.vue";
 export { default as ReviewDetailKpiGrid } from "./components/ReviewDetailKpiGrid.vue";
 export { default as ReviewDetailSidePanel } from "./components/ReviewDetailSidePanel.vue";
 export { default as ReviewDetailSummaryCard } from "./components/ReviewDetailSummaryCard.vue";
