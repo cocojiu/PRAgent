01aa79119f1b0148824142e90fe23f5796af35fd
cocoyu <874522742@qq.com>
2026-07-06 15:07:25 +0800

    拆分审查执行工作流装配边界

---
+++ repoguard-backend/src/main/java/com/repoguard/agent/worker/ReviewExecutionWorkflowFactory.java
@@ -0,0 +1,109 @@
+package com.repoguard.agent.worker;
+
+import com.repoguard.agent.github.GithubPullRequestClient;
+import com.repoguard.agent.mapper.ChangedFileMapper;
+import com.repoguard.agent.mapper.ReviewFindingMapper;
+import com.repoguard.agent.mapper.ReviewTaskMapper;
+import com.repoguard.agent.mapper.ReviewTimelineMapper;
+import com.repoguard.agent.notification.NotificationDispatchService;
+import com.repoguard.agent.observability.RepoGuardMetrics;
+import com.repoguard.agent.review.PullRequestReviewer;
+import com.repoguard.agent.review.ReviewTaskStateMachine;
+import com.repoguard.agent.review.RiskLevelRanker;
+import org.springframework.transaction.PlatformTransactionManager;
+
+class ReviewExecutionWorkflowFactory {
+
+    ReviewExecutionWorkflow create(
+        ReviewTaskMapper reviewTaskMapper,
+        ReviewTimelineMapper reviewTimelineMapper,
+        ReviewFindingMapper reviewFindingMapper,
+        ChangedFileMapper changedFileMapper,
+        GithubPullRequestClient githubPullRequestClient,
+        PullRequestReviewer pullRequestReviewer,
+        PlatformTransactionManager transactionManager,
+        RepoGuardMetrics metrics,
+        NotificationDispatchService notificationDispatchService
+    ) {
+        ReviewExecutionClock clock = new ReviewExecutionClock();
+        ReviewLogContextFormatter logContextFormatter = new ReviewLogContextFormatter();
+        RiskLevelRanker riskLevelRanker = new RiskLevelRanker();
+        ReviewTaskStateMachine reviewTaskStateMachine = new ReviewTaskStateMachine();
+        ReviewFindingDeduplicator findingDeduplicator = new ReviewFindingDeduplicator(
+            new ReviewFindingDeduplicationKeyResolver(),
+            new ReviewFindingMergeService(riskLevelRanker)
+        );
+        ReviewTimelineAppender timelineAppender = new ReviewTimelineAppender(reviewTimelineMapper);
+        ReviewExecutionTimelineRecorder timelineRecorder = new ReviewExecutionTimelineRecorder(timelineAppender);
+        ChangedFileReplacementService changedFileReplacementService = new ChangedFileReplacementService(
+            changedFileMapper,
+            new ChangedFileEntityMapper()
+        );
+        ReviewFindingReplacementService findingReplacementService = new ReviewFindingReplacementService(
+            reviewFindingMapper,
+            findingDeduplicator,
+            new ReviewFindingEntityMapper()
+        );
+        ReviewTaskCompletionApplier completionApplier = new ReviewTaskCompletionApplier(
+            reviewTaskStateMachine,
+            new ReviewHumanReviewDecisionPolicy(riskLevelRanker),
+            new ReviewTaskFailureOutcomePolicy(),
+            new ReviewTaskDurationPolicy()
+        );
+        ReviewTaskClaimService claimService = new ReviewTaskClaimService(reviewTaskMapper, reviewTaskStateMachine);
+        ReviewExecutionMetricsRecorder metricsRecorder = new ReviewExecutionMetricsRecorder(metrics);
+        ReviewExecutionFailureClassifier failureClassifier = new ReviewExecutionFailureClassifier();
+        ReviewExecutionCacheInvalidator cacheInvalidator = new ReviewExecutionCacheInvalidator(
+            new ReviewExecutionNoopCacheEvictionService()
+        );
+        ReviewExecutionFailureHandler failureHandler = new ReviewExecutionFailureHandler(
+            reviewTaskMapper,
+            claimService,
+            completionApplier,
+            timelineRecorder,
+            metricsRecorder,
+            cacheInvalidator,
+            clock,
+            failureClassifier
+        );
+        ReviewExecutionResultWriter resultWriter = new ReviewExecutionResultWriter(
+            reviewTaskMapper,
+            claimService,
+            completionApplier,
+            changedFileReplacementService,
+            findingReplacementService,
+            timelineRecorder,
+            metricsRecorder,
+            cacheInvalidator,
+            clock
+        );
+        return new ReviewExecutionWorkflow(
+            pullRequestReviewer,
+            new ReviewExecutionTransactionRunner(transactionManager, 3),
+            new GithubPullRequestDiffFetcher(
+                githubPullRequestClient,
+                metricsRecorder,
+                clock,
+                logContextFormatter,
+                failureClassifier
+            ),
+            reviewTaskStateMachine,
+            timelineRecorder,
+            claimService,
+            failureHandler,
+            resultWriter,
+            new ReviewExecutionNotifier(notificationDispatchServiceOrNoop(notificationDispatchService)),
+            new ReviewExecutionDiffStats(),
+            new ReviewExecutionLog(clock, logContextFormatter),
+            clock
+        );
+    }
+
+    private NotificationDispatchService notificationDispatchServiceOrNoop(
+        NotificationDispatchService notificationDispatchService
+    ) {
+        return notificationDispatchService == null
+            ? new ReviewExecutionNoopNotificationDispatchService()
+            : notificationDispatchService;
+    }
+}
--- repoguard-backend/src/main/java/com/repoguard/agent/worker/ReviewTaskExecutorImpl.java
+++ repoguard-backend/src/main/java/com/repoguard/agent/worker/ReviewTaskExecutorImpl.java
@@ -10,8 +10,6 @@ import com.repoguard.agent.messaging.ReviewTaskMessage;
 import com.repoguard.agent.notification.NotificationDispatchService;
 import com.repoguard.agent.observability.RepoGuardMetrics;
 import com.repoguard.agent.review.PullRequestReviewer;
-import com.repoguard.agent.review.ReviewTaskStateMachine;
-import com.repoguard.agent.review.RiskLevelRanker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -41,7 +39,7 @@ public class ReviewTaskExecutorImpl implements ReviewTaskExecutor {
     ) {
         this(
             reviewTaskMapper,
-            createWorkflow(
+            new ReviewExecutionWorkflowFactory().create(
                 reviewTaskMapper,
                 reviewTimelineMapper,
                 reviewFindingMapper,
@@ -103,98 +101,5 @@ public class ReviewTaskExecutorImpl implements ReviewTaskExecutor {
     public void execute(ReviewTaskMessage message) {
         ReviewTask task = reviewTaskMapper.selectById(message.taskId());
         workflow.execute(message, task);
-    }
-
-    private static ReviewExecutionWorkflow createWorkflow(
-        ReviewTaskMapper reviewTaskMapper,
-        ReviewTimelineMapper reviewTimelineMapper,
-        ReviewFindingMapper reviewFindingMapper,
-        ChangedFileMapper changedFileMapper,
-        GithubPullRequestClient githubPullRequestClient,
-        PullRequestReviewer pullRequestReviewer,
-        PlatformTransactionManager transactionManager,
-        RepoGuardMetrics metrics,
-        NotificationDispatchService notificationDispatchService
-    ) {
-        ReviewExecutionClock clock = new ReviewExecutionClock();
-        ReviewLogContextFormatter logContextFormatter = new ReviewLogContextFormatter();
-        RiskLevelRanker riskLevelRanker = new RiskLevelRanker();
-        ReviewTaskStateMachine reviewTaskStateMachine = new ReviewTaskStateMachine();
-        ReviewFindingDeduplicator findingDeduplicator = new ReviewFindingDeduplicator(
-            new ReviewFindingDeduplicationKeyResolver(),
-            new ReviewFindingMergeService(riskLevelRanker)
-        );
-        ReviewTimelineAppender timelineAppender = new ReviewTimelineAppender(reviewTimelineMapper);
-        ReviewExecutionTimelineRecorder timelineRecorder = new ReviewExecutionTimelineRecorder(timelineAppender);
-        ChangedFileReplacementService changedFileReplacementService = new ChangedFileReplacementService(
-            changedFileMapper,
-            new ChangedFileEntityMapper()
-        );
-        ReviewFindingReplacementService findingReplacementService = new ReviewFindingReplacementService(
-            reviewFindingMapper,
-            findingDeduplicator,
-            new ReviewFindingEntityMapper()
-        );
-        ReviewTaskCompletionApplier completionApplier = new ReviewTaskCompletionApplier(
-            reviewTaskStateMachine,
-            new ReviewHumanReviewDecisionPolicy(riskLevelRanker),
-            new ReviewTaskFailureOutcomePolicy(),
-            new ReviewTaskDurationPolicy()
-        );
-        ReviewTaskClaimService claimService = new ReviewTaskClaimService(reviewTaskMapper, reviewTaskStateMachine);
-        ReviewExecutionMetricsRecorder metricsRecorder = new ReviewExecutionMetricsRecorder(metrics);
-        ReviewExecutionFailureClassifier failureClassifier = new ReviewExecutionFailureClassifier();
-        ReviewExecutionCacheInvalidator cacheInvalidator = new ReviewExecutionCacheInvalidator(
-            new ReviewExecutionNoopCacheEvictionService()
-        );
-        ReviewExecutionFailureHandler failureHandler = new ReviewExecutionFailureHandler(
-            reviewTaskMapper,
-            claimService,
-            completionApplier,
-            timelineRecorder,
-            metricsRecorder,
-            cacheInvalidator,
-            clock,
-            failureClassifier
-        );
-        ReviewExecutionResultWriter resultWriter = new ReviewExecutionResultWriter(
-            reviewTaskMapper,
-            claimService,
-            completionApplier,
-            changedFileReplacementService,
-            findingReplacementService,
-            timelineRecorder,
-            metricsRecorder,
-            cacheInvalidator,
-            clock
-        );
-        return new ReviewExecutionWorkflow(
-            pullRequestReviewer,
-            new ReviewExecutionTransactionRunner(transactionManager, 3),
-            new GithubPullRequestDiffFetcher(
-                githubPullRequestClient,
-                metricsRecorder,
-                clock,
-                logContextFormatter,
-                failureClassifier
-            ),
-            reviewTaskStateMachine,
-            timelineRecorder,
-            claimService,
-            failureHandler,
-            resultWriter,
-            new ReviewExecutionNotifier(notificationDispatchServiceOrNoop(notificationDispatchService)),
-            new ReviewExecutionDiffStats(),
-            new ReviewExecutionLog(clock, logContextFormatter),
-            clock
-        );
-    }
-
-    private static NotificationDispatchService notificationDispatchServiceOrNoop(
-        NotificationDispatchService notificationDispatchService
-    ) {
-        return notificationDispatchService == null
-            ? new ReviewExecutionNoopNotificationDispatchService()
-            : notificationDispatchService;
     }
 }
